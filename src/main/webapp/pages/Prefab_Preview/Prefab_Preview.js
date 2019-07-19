73f28d3b3219ed6c8a1763e1d9a53ccafd47123e
raajvamsy <raajvamsy.rayavarapu@imaginea.com>
2019-07-19 09:03:51 +0000

    Preview commit New files added - [default,fonts] Files modified - [Prefab_Preview,.wmproject.properties,dependencies.json,index.html] Removed files - [fonts,material]

--- src/main/webapp/pages/Prefab_Preview/Prefab_Preview.js
+++ src/main/webapp/pages/Prefab_Preview/Prefab_Preview.js
@@ -14,8 +14,15 @@ Page.onReady = function() {
      * e.g. to get value of text widget named 'username' use following script
      * 'Page.Widgets.username.datavalue'
      */
-
+    Page.Widgets.save.show = false;
+    Page.Widgets.save1.show = false;
+    Page.Widgets.cancel.show = false;
+    Page.Widgets.cancel1.show = false;
 };
+
+function nameEdit() {
+    alert("nameEdit")
+}
 var jsondata = {
     "Name": "PrefabName",
     "icon": "https://media.licdn.com/dms/image/C560BAQE9O_4idOVN0w/company-logo_200_200/0?e=2159024400&v=beta&t=NmS2mTzVU8MSm-UkeRwc4vFqyYaEoD2VMMpqSt5eEsg",
@@ -34,3 +41,50 @@ var jsondata = {
     "Description": "Prefab  Description",
 
 }
+var temp;
+Page.editClick = function($event, widget) {
+    Page.Widgets.text2.readonly = false;
+    Page.Widgets.edit.show = false;
+    Page.Widgets.save.show = true;
+    Page.Widgets.cancel.show = true;
+    temp = Page.Widgets.text2.datavalue;
+};
+Page.saveClick = function($event, widget) {
+    Page.Widgets.text2.readonly = true;
+    Page.Widgets.save.show = false;
+    Page.Widgets.cancel.show = false;
+    Page.Widgets.edit.show = true;
+};
+Page.cancelClick = function($event, widget) {
+    Page.Widgets.text2.datavalue = temp;
+    temp = null;
+    Page.Widgets.text2.readonly = true;
+    Page.Widgets.save.show = false;
+    Page.Widgets.cancel.show = false;
+    Page.Widgets.edit.show = true;
+
+};
+Page.edit1Click = function($event, widget) {
+    Page.Widgets.textarea2.readonly = false;
+    Page.Widgets.edit1.show = false;
+    Page.Widgets.save1.show = true;
+    Page.Widgets.cancel1.show = true;
+    temp = Page.Widgets.textarea2.datavalue;
+};
+
+Page.save1Click = function($event, widget) {
+    Page.Widgets.textarea2.readonly = true;
+    Page.Widgets.save1.show = false;
+    Page.Widgets.cancel1.show = false;
+    Page.Widgets.edit1.show = true;
+};
+
+Page.cancel1Click = function($event, widget) {
+    Page.Widgets.textarea2.datavalue = temp;
+    temp = null;
+    Page.Widgets.textarea2.readonly = true;
+    Page.Widgets.save1.show = false;
+    Page.Widgets.cancel1.show = false;
+    Page.Widgets.edit1.show = true;
+
+};
