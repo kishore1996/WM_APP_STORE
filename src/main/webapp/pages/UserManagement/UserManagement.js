a92ef214d02acfd007a5857204a2d276d2a6b75b
Deepak NN <deepak.narayanan@imaginea.com>
2019-07-19 06:12:54 +0000

    commit user management New files added - [UserManagement] Files modified - [Login,Main,header,WM_APP_STORE,securityService,profiles,app.variables.json,pages-config.json]

--- src/main/webapp/pages/UserManagement/UserManagement.js
+++ src/main/webapp/pages/UserManagement/UserManagement.js
@@ -3,16 +3,18 @@
  * eg: var DialogService = App.getDependency('DialogService');
  */
 
-
 /* perform any action on widgets/variables within this block */
-Partial.onReady = function() {
+Page.onReady = function() {
     /*
-     * variables can be accessed through 'Partial.Variables' property here
+     * variables can be accessed through 'Page.Variables' property here
      * e.g. to get dataSet in a staticVariable named 'loggedInUser' use following script
-     * Partial.Variables.loggedInUser.getData()
+     * Page.Variables.loggedInUser.getData()
      *
-     * widgets can be accessed through 'Partial.Widgets' property here
+     * widgets can be accessed through 'Page.Widgets' property here
      * e.g. to get value of text widget named 'username' use following script
-     * 'Partial.Widgets.username.datavalue'
+     * 'Page.Widgets.username.datavalue'
      */
+};
+Page.tabpane2Select = function($event, widget) {
+    Page.Variables.viewUsersVariable.invoke();
 };
