0e4a6018f536f0a97be5db693a6e127f9579b125
Deepak NN <deepak.narayanan@imaginea.com>
2019-07-19 09:22:31 +0000

    commit from deepak New files added - [viewProfile] Files modified - [Login,UserManagement,topnav,WM_APP_STORE,securityService,profiles,project-security.xml,app.variables.json,pages-config.json]

--- services/WM_APP_STORE/src/com/wm_app_store/wm_app_store/controller/QueryExecutionController.java
+++ services/WM_APP_STORE/src/com/wm_app_store/wm_app_store/controller/QueryExecutionController.java
@@ -12,20 +12,26 @@ import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wavemaker.commons.wrapper.IntegerWrapper;
+import com.wavemaker.commons.wrapper.StringWrapper;
+import com.wavemaker.runtime.data.export.ExportOptions;
 import com.wavemaker.runtime.file.manager.ExportedFileManager;
+import com.wavemaker.runtime.security.xss.XssDisable;
 import com.wavemaker.tools.api.core.annotations.WMAccessVisibility;
 import com.wavemaker.tools.api.core.models.AccessSpecifier;
 import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
 
-import com.wm_app_store.wm_app_store.models.query.EditUserRequest;
 import com.wm_app_store.wm_app_store.service.WM_APP_STOREQueryExecutorService;
+import com.wm_app_store.wm_app_store.models.query.*;
 
 @RestController(value = "WM_APP_STORE.QueryExecutionController")
 @RequestMapping("/WM_APP_STORE/queryExecutor")
@@ -40,6 +46,16 @@ public class QueryExecutionController {
     @Autowired
 	private ExportedFileManager exportedFileManager;
 
+    @RequestMapping(value = "/queries/deleteUser", method = RequestMethod.DELETE)
+    @WMAccessVisibility(value = AccessSpecifier.APP_ONLY)
+    @ApiOperation(value = "delete users")
+    public IntegerWrapper executeDeleteUser(@RequestParam(value = "uname") String uname, @RequestParam(value = "id") Integer id, @RequestParam(value = "loggedinuser") String loggedinuser, HttpServletRequest _request) {
+        LOGGER.debug("Executing named query: deleteUser");
+        Integer _result = queryService.executeDeleteUser(uname, id, loggedinuser);
+        LOGGER.debug("got the result for named query: deleteUser, result:{}", _result);
+        return new IntegerWrapper(_result);
+    }
+
     @RequestMapping(value = "/queries/editUser", method = RequestMethod.PUT)
     @WMAccessVisibility(value = AccessSpecifier.APP_ONLY)
     @ApiOperation(value = "Edit user credentials")
@@ -47,6 +63,45 @@ public class QueryExecutionController {
         LOGGER.debug("Executing named query: editUser");
         Integer _result = queryService.executeEditUser(editUserRequest);
         LOGGER.debug("got the result for named query: editUser, result:{}", _result);
+        return new IntegerWrapper(_result);
+    }
+
+    @RequestMapping(value = "/queries/viewProfile", method = RequestMethod.GET)
+    @WMAccessVisibility(value = AccessSpecifier.APP_ONLY)
+    @ApiOperation(value = "view logged user profile")
+    public Page<ViewProfileResponse> executeViewProfile(@RequestParam(value = "uname") String uname, Pageable pageable, HttpServletRequest _request) {
+        LOGGER.debug("Executing named query: viewProfile");
+        Page<ViewProfileResponse> _result = queryService.executeViewProfile(uname, pageable);
+        LOGGER.debug("got the result for named query: viewProfile, result:{}", _result);
+        return _result;
+    }
+
+    @ApiOperation(value = "Returns downloadable file url for query viewProfile")
+    @RequestMapping(value = "/queries/viewProfile/export", method = RequestMethod.POST)
+    @WMAccessVisibility(value = AccessSpecifier.APP_ONLY)
+    @XssDisable
+    public StringWrapper exportViewProfile(@RequestParam(value = "uname") String uname, @RequestBody ExportOptions exportOptions, Pageable pageable) {
+        LOGGER.debug("Exporting named query: viewProfile");
+
+        String exportedFileName = exportOptions.getFileName();
+        if(exportedFileName == null || exportedFileName.isEmpty()) {
+            exportedFileName = "viewProfile";
+        }
+        exportedFileName += exportOptions.getExportType().getExtension();
+
+        String exportedUrl = exportedFileManager.registerAndGetURL(exportedFileName,
+                        outputStream -> queryService.exportViewProfile(uname,  exportOptions, pageable, outputStream));
+
+        return new StringWrapper(exportedUrl);
+    }
+
+    @RequestMapping(value = "/queries/changePassword", method = RequestMethod.PUT)
+    @WMAccessVisibility(value = AccessSpecifier.APP_ONLY)
+    @ApiOperation(value = "change user password")
+    public IntegerWrapper executeChangePassword(@Valid @RequestBody ChangePasswordRequest changePasswordRequest, HttpServletRequest _request) {
+        LOGGER.debug("Executing named query: changePassword");
+        Integer _result = queryService.executeChangePassword(changePasswordRequest);
+        LOGGER.debug("got the result for named query: changePassword, result:{}", _result);
         return new IntegerWrapper(_result);
     }
 
---
+++ src/main/webapp/pages/viewProfile/viewProfile.js
@@ -0,0 +1,33 @@
+/*
+ * Use App.getDependency for Dependency Injection
+ * eg: var DialogService = App.getDependency('DialogService');
+ */
+var user = Page.App.Variables.loggedInUser.getData()
+/* perform any action on widgets/variables within this block */
+Page.onReady = function() {
+    /*
+     * variables can be accessed through 'Page.Variables' property here
+     * e.g. to get dataSet in a staticVariable named 'loggedInUser' use following script
+     * Page.Variables.loggedInUser.getData()
+     *
+     * widgets can be accessed through 'Page.Widgets' property here
+     * e.g. to get value of text widget named 'username' use following script
+     * 'Page.Widgets.username.datavalue'
+     */
+    console.log(user["name"])
+    Page.Variables.viewProfileVariable.setInput({
+        "uname": user["name"]
+    })
+    Page.Variables.viewProfileVariable.invoke()
+
+};
+
+Page.tabpane1Select = function($event, widget) {
+    Page.Variables.viewProfileVariable.setInput({
+        "uname": user["name"]
+    })
+    Page.Variables.viewProfileVariable.invoke()
+};
+Page.tabpane2Select = function($event, widget) {
+
+};
