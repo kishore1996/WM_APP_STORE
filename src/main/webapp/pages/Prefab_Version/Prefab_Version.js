269d1f4f8ee80b0f9637259a70067d765e966765
raajvamsy <raajvamsy.rayavarapu@imaginea.com>
2019-07-24 08:05:43 +0000

    Page _ version changes commit Files modified - [Prefab_Preview,Prefab_Version,pom.xml]

--- src/main/webapp/pages/Prefab_Version/Prefab_Version.js
+++ src/main/webapp/pages/Prefab_Version/Prefab_Version.js
@@ -32,11 +32,13 @@ Page.insertInto_appscreenshotsonSuccess = function(variable, data) {
     else
         check = 0;
 };
-
+var screencheck = false
 Page.FileServiceUploadFile1onSuccess = function(variable, data) {
     //console.log(data)
     var i = 0;
     var flag = 0;
+    console.log("len")
+    console.log(data.length)
     var inter = setInterval(function() {
         if (flag == 0) {
             Page.Variables.insert_screenshots.setInput({
@@ -50,41 +52,56 @@ Page.FileServiceUploadFile1onSuccess = function(variable, data) {
             flag = 0
             check = 0
         }
+        console.log(i)
         if (i == data.length) {
+            screencheck = true
             clearInterval(inter);
         }
     }, 1000)
 };
-
+Page.save_buttonClick = function($event, widget) {
+    if (Page.Widgets.description.datavalue == null) {
+        Page.Actions.nullnotify.invoke();
+    } else if (Page.Widgets.prefabupload.selectedFiles.length == 0) {
+        Page.Actions.fileupload.invoke();
+    } else if (Page.Widgets.screenshotsupload.selectedFiles.length == 0) {
+        Page.Actions.fileupload1.invoke();
+    } else if (!screencheck) {
+        console.log("wait till screenshotsupload")
+    } else if (!prefabcheck) {
+        console.log("wait till prefabc upload")
+    } else {
+        if (Page.Variables.FileServiceUploadFile.dataSet.length > 0) {
+            data = Page.Variables.FileServiceUploadFile.dataSet
+            d = Page.Variables.database_appsource.dataSet[0]
+            d["fileName"] = data[0]["fileName"]
+            d["filePath"] = data[0]["inlinePath"]
+            if (d["versionId"] == null) {
+                d["versionId"] = 1
+            } else {
+                d["versionId"] = d["versionId"] + 1
+            }
+            Page.Variables.insert_appsource.createRecord({
+                row: d
+            })
+            // Page.Variables.update_appinfo.invoke();
+            Page.Actions.success.invoke();
+        }
+    }
+};
 
 var no;
+var prefabcheck = false
 Page.database_appsourceonSuccess = function(variable, data) {};
 Page.FileServiceUploadFileonSuccess = function(variable, data) {
     console.log("FIle")
     console.log(data)
-    Page.save_buttonClick = function($event, widget) {
-        if (Page.Widgets.description.datavalue == null) {
-            Page.Actions.nullnotify.invoke();
-        } else if (Page.Widgets.prefabupload.selectedFiles.length == 0) {
-            Page.Actions.fileupload.invoke();
-        } else if (Page.Widgets.screenshotsupload.selectedFiles.length == 0) {
-            Page.Actions.fileupload1.invoke();
-        } else {
-            if (Page.Variables.FileServiceUploadFile.dataSet.length > 0) {
-                d = Page.Variables.database_appsource.dataSet[0]
-                d["fileName"] = data[0]["fileName"]
-                d["filePath"] = data[0]["inlinePath"]
-                if (d["versionId"] == null) {
-                    d["versionId"] = 1
-                } else {
-                    d["versionId"] = d["versionId"] + 1
-                }
-                Page.Variables.insert_appsource.createRecord({
-                    row: d
-                })
-                // Page.Variables.update_appinfo.invoke();
-                Page.Actions.success.invoke();
-            }
-        }
-    };
+    prefabcheck = true;
+};
+
+Page.insert_screenshotsonSuccess = function(variable, data) {
+    if (data != null)
+        check = 1;
+    else
+        check = 0;
 };
